586ecf7638d878fa023e20f40005a31f68cb3685
Patrick Beuhler <patrickbeuhler@gmail.com>
2021-03-31 19:43:22 -0700

    board

---
+++ src/components/Board.tsx
@@ -0,0 +1,221 @@
+import React from "react";
+
+import "./Board.css";
+import Square from "./Square";
+
+function Board() {
+  return (
+    <div className="board">
+      <div className="column">
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+      </div>
+      <div className="column">
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+      </div>
+      <div className="column">
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+      </div>
+      <div className="column">
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+      </div>
+      <div className="column">
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+      </div>
+      <div className="column">
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+      </div>
+      <div className="column">
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+      </div>
+      <div className="column">
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+        <div className="row">
+          <Square />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/components/Square.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Square() {
+  return <div className="square"></div>;
+}
+
+export default Square;
